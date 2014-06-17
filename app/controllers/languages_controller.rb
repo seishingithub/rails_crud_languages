557b10e3f4aa0f56dd5c8a895e9f46798a38758d
Peggy Griffin <seishingithub@gmail.com>
2014-06-16 22:18:51 -0600

    User can edit and update languages from a list

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -14,6 +14,21 @@ class LanguagesController < ApplicationController
     end
   end
 
+  def show
+    @language = Language.find(params[:id])
+  end
+
+  def edit
+    @language = Language.find(params[:id])
+  end
+
+  def update
+    @language = Language.find(params[:id])
+    @language.update_attributes!(language_params)
+
+    redirect_to languages_path
+  end
+
   private
   def language_params
     params.require(:language).permit(:name_of_language, :country_spoken)
--- spec/features/languages_spec.rb
+++ spec/features/languages_spec.rb
@@ -11,4 +11,26 @@ feature 'Manage Crud' do
     expect(page).to have_content 'French'
     expect(page).to have_content 'France'
   end
+
+  scenario 'User can edit and update list of languages' do
+    visit '/'
+    click_on 'Add a language'
+    fill_in 'Name of language', with: 'French'
+    fill_in 'Country spoken', with: 'France'
+    click_on 'Create language'
+    expect(page).to have_content 'French'
+    expect(page).to have_content 'France'
+    click_on 'French'
+    expect(page).to have_content 'French'
+    expect(page).to have_content 'France'
+    click_on 'Edit language'
+    fill_in 'Name of language', with: 'Portuguese'
+    fill_in 'Country spoken', with: 'Brazil'
+    click_on 'Update language'
+    expect(page).to have_no_content 'French'
+    expect(page).to have_no_content 'France'
+    expect(page).to have_content 'Portuguese'
+    expect(page).to have_content 'Brazil'
+
+  end
 end
